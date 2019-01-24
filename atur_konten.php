af8838a5fdacc4d9dc593aa8aa35185ecc316f7c
writerlab <netspytux@gmail.com>
2019-01-24 09:25:26 +0700

    first commit

---
+++ atur_konten.php
@@ -0,0 +1,8 @@
+<?php
+
+if ($_GET['menu'] == dashboard) {
+  print "<h1>ini halaman dashboard</h1>";
+}
+elseif ($_GET['menu'] == inventaris) {
+  print "<h1>INVENTARIS...</h1>";
+}
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+include('konten/header.html');
+include('konten/nav.html'); ?>
+
+    <div id="wrapper">
+
+      <?php include('konten/sidebar.html');?>
+
+      <div id="content-wrapper">
+        <div class="container-fluid">
+          
+          <?php include('atur_konten.php'); ?>
+        
+        </div>
+        <!-- /.container-fluid -->
+      </div>
+      <!-- /.content-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+
+<?php
+include('konten/footer.html'); ?>
